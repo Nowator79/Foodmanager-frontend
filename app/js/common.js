693ff5c242e2326e694f0481617918bc2ef4e4f9
Vadim Radium <56523736+Nowator79@users.noreply.github.com>
2022-06-27 20:06:46 +0900

    27062022

--- app/js/common.js
+++ app/js/common.js
@@ -5,6 +5,29 @@ $( document ).ready(function() {
 			0:{
 					items:1
 			}
-	}
-	})
+		}
+	});
+
+
+	var modal_bg = $(".modal_bg");
+
+	modal_bg.click(function(e){
+		e.preventDefault();
+		$(".modal_js").removeClass("active");
+		modal_bg.removeClass("active");
+	});
+
+	$("[hendler='modal']").click(function(e){
+		e.preventDefault();
+		var those = $(this);
+		var data = those.attr("data");
+		var target = 	$(data);
+		if(modal_bg.hasClass("active")){
+			target.removeClass("active");
+			modal_bg.removeClass("active");
+		}else{
+			target.addClass("active");
+			modal_bg.addClass("active");
+		}
+	});
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,33 @@
 import pkg from 'gulp'
 const { gulp, src, dest, parallel, series, watch } = pkg
 
-import browserSync   from 'browser-sync'
-import gulpSass      from 'gulp-sass'
-import dartSass      from 'sass'
-import postCss       from 'gulp-postcss'
-import cssnano       from 'cssnano'
-const  sassfn        = gulpSass(dartSass)
-import concat        from 'gulp-concat'
-import uglifyim      from 'gulp-uglify-es'
-const  uglify        = uglifyim.default
-import rename        from 'gulp-rename'
-import del           from 'del'
-import imageminfn    from 'gulp-imagemin'
-import cache         from 'gulp-cache'
-import autoprefixer  from 'autoprefixer'
-import ftp           from 'vinyl-ftp'
-import rsyncfn       from 'gulp-rsync'
+import browserSync  	from 'browser-sync'
+import gulpSass     	from 'gulp-sass'
+import dartSass     	from 'sass'
+import postCss      	from 'gulp-postcss'
+import cssnano      	from 'cssnano'
+const  sassfn       	= gulpSass(dartSass)
+import concat       	from 'gulp-concat'
+import uglifyim     	from 'gulp-uglify-es'
+const  uglify       	= uglifyim.default
+import rename       	from 'gulp-rename'
+import del          	from 'del'
+import imageminfn   	from 'gulp-imagemin'
+import cache        	from 'gulp-cache'
+import autoprefixer 	from 'autoprefixer'
+import ftp          	from 'vinyl-ftp'
+import rsyncfn      	from 'gulp-rsync'
+import fetch					from 'node-fetch'
+import fs							from 'fs'
+import FormData 			from 'form-data'
+
+var url = 'http://vm-0b5feeaf.na4u.ru/api/setFile.php';
+
+
+var href_styles = "app/css/template_styles.min.css";
+var href_scripts = "app/js/scripts.min.js";
+var path_styles = "/assets/css/template_styles.min.css";
+var path_scripts = "/assets/js/scripts.min.js";
 
 function browsersync() {
 	browserSync.init({
@@ -33,6 +44,7 @@ function browsersync() {
 function js() {
 	return src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/bootstrap/bootstrap.js',
 		'app/libs/owl-carousel/owl.carousel.min.js', 
 		'app/js/common.js', // Всегда в конце
 		])
@@ -55,6 +67,8 @@ function Mainsass() {
 }
 
 function Templatesass() {
+
+
 	return src('app/sass/**/template_styles.sass')
 	.pipe(sassfn())
 	.pipe(postCss([
@@ -64,6 +78,10 @@ function Templatesass() {
 	.pipe(rename({ suffix: '.min', prefix : '' }))
 	.pipe(dest('app/css'))
 	.pipe(browserSync.stream())
+}
+function UpdateSass(){
+	var data = Templatesass();
+	return data;
 }
 
 function imagemin() {
@@ -119,13 +137,39 @@ function rsync() {
 function startwatch() {
 	watch('app/sass/**/main.sass', { usePolling: true }, Mainsass)
 	watch('app/sass/**/template_styles.sass', { usePolling: true }, Mainsass)
-	watch('app/sass/**/style.scss', { usePolling: true }, Templatesass)
+	watch('app/sass/**/style.scss', { usePolling: true }, UpdateSass)
 
 	watch(['libs/**/*.js', 'app/js/common.js'], { usePolling: true }, js)
 	watch(['app/*.html'], { usePolling: true }).on('change', browserSync.reload)
+	watch([href_styles], { usePolling: true }).on('change', StylesRequestSave)
+	watch([href_scripts], { usePolling: true }).on('change', ScriptsRequestSave)
 }
 
-export { js, Mainsass, imagemin, deploy, rsync, clearcache }
-export let build = series(removedist, imagemin, js, Mainsass, Templatesass, buildcopy)
+function StylesRequestSave(){
 
-export default series(js, Mainsass, Templatesass, parallel(browsersync, startwatch))
+	let data = new FormData();
+	data.append('file_path', path_styles);
+	data.append('file', fs.createReadStream(href_styles));
+	
+	fetch(url, {
+		method: 'POST',
+		body: data
+	})
+	.then();
+}
+function ScriptsRequestSave(){
+
+	let data = new FormData();
+	data.append('file_path', path_scripts);
+	data.append('file', fs.createReadStream(href_scripts));
+	
+	fetch(url, {
+		method: 'POST',
+		body: data
+	})
+	.then();
+}
+export { js, Mainsass, imagemin, deploy, rsync, clearcache }
+export let build = series(removedist, imagemin, js, Mainsass, UpdateSass, buildcopy)
+
+export default series(js, Mainsass, UpdateSass, parallel(browsersync, startwatch))
